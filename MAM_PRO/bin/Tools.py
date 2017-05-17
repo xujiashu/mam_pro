d0af85628c88029627ad304ccf49cfbdcc906fac
xujiashu <13760701536@163.com>
2017-05-17 21:08:22 +0800

    add the Tools founction

--- MAM_PRO/bin/Tools.py
+++ MAM_PRO/bin/Tools.py
@@ -1,25 +1,58 @@
+import xlrd
+import os
+
+
 '''
 create a group of tools to analysis the excel file.
 
 '''
 
-def anal_excel(file_name):
+
+def create_com_list(file_name):
 '''
-	to read the excel file and return a data list
+	to read the excel file and return a com_list
 '''
-	pass
+	com_list = []
+	wb = xlrd.open_workbook(file_name)
+	sheet1 = wb.sheet_by_index(0)
+	for cow in range(sheet1.nrows):
+		com_list.append(cow)
+	return com_list
 
 
-def write(file, oklist, notok, remain):
-'''
-	save the result in a file
-'''
-	pass
+def match(msg, filename):
+	flag = True
+	msg = msg[0:4]
+	for item in msg:
+		if item in filname:
+			continue
+		else:
+			flag = False
+	return flag
 
-def receive():
+
+def compare(rec_list, com_list):
 '''
-	collect the file name that receive from email.
+wheather the rec_list item in com_list
+return suc_list and return the remain item in com_list
 '''
 
+	suc_list = []
+	for filename in rec_list:
+		for msg in com_list:
+			if match(msg, filename):
+				suc_list.append(filename)
+				com_list.pop(msg)
+	remain_list = com_list
+	print('remain students:')
+	for i in range(len(remain_list)):
+		remain_list[i] = ','.join(remain_list[i])
+	
+	return remain_list
 
+
+def create_rec_list(path='./')
+	dir_list = os.listdir(path)
+	rec_list = [f for f in dir_list if f.split('.')[1]=='pdf']
+	return rec_list
 
